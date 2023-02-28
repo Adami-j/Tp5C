db4ee96ed94f5fb2939560173d4d3b1504efc770
Julien ADAMI <adamij@3il.fr>
2023-02-28 13:55:43 +0100

    EX1 TP5 lecture d'un fichier  + ajout du fichier d'écriture

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char nomFichier[100] = "C:\\Users\\mkltr\\CLionProjects\\Tp5\\testecriture";
+
+    FILE *file;
+    file = fopen(nomFichier,"r");
+    char fileContent[100]="";
+    if (file== NULL) {
+        printf("Error: File not found.");
+        return 1;
+    }
+    while (fgets(fileContent, 100, file) != NULL){
+        printf("%s",fileContent);
+    }
     return 0;
 }
