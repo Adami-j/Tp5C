f92c1c10f3513384845f76b02450dda83553ce6d
Julien ADAMI <adamij@3il.fr>
2023-02-28 14:23:44 +0100

    EX1 TP5 ça fonctionne

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    char nomFichier[100] = "C:\\Users\\mkltr\\CLionProjects\\Tp5\\testecriture";
+int main(int args) {
+    char nomFichier[100] = "C:/Users/mkltr/CLionProjects/Tp5/testecriture";
 
     FILE *file;
     file = fopen(nomFichier,"r");
@@ -10,8 +11,23 @@ int main() {
         printf("Error: File not found.");
         return 1;
     }
+
     while (fgets(fileContent, 100, file) != NULL){
         printf("%s",fileContent);
     }
+
+    FILE *fileWriting;
+
+    fileWriting= fopen(nomFichier,"w");
+
+    char chosesAEcrire[]="";
+    printf("Saisis ce que tu veux écrire dans ce fichier");
+
+    fgets(chosesAEcrire,100,stdin);
+
+    fprintf(fileWriting,"%s",chosesAEcrire);
+
+    fclose(file);
+    fclose(fileWriting);
     return 0;
 }
